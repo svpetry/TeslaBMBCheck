c274dfa6f921c0e55c15f6bff42556822cf85eda
SP <svpetry@gmail.com>
2025-07-12 17:58:22 +0200

    MCU: Adds missing files

---
+++ MCU/TestlaBMBCheck.X/board.c
@@ -0,0 +1,34 @@
+#include <pic18.h>
+#include "base.h"
+#include "types.h"
+
+#include "board.h"
+
+
+void WaitButtonsReleased() {
+    while (!PORTAbits.RA0 || !PORTAbits.RA1) ;
+}
+
+void WaitForButtonPressed() {
+    while (PORTAbits.RA0 && PORTAbits.RA1) ;
+}
+
+bool GetBtnState(uint8_t id) {
+    if (id == 0)
+        return PORTAbits.RA0;
+    else
+        return PORTAbits.RA1;
+}
+
+void SetInfoLed(bool state) {
+    LATCbits.LATC2 = state;
+}
+
+void SetLcdBacklight(bool state) {
+    LATCbits.LATC1 = state;
+}
+
+void SetBmsPower(bool state) {
+    LATCbits.LATC0 = state ? 0 : 1;
+}
+
